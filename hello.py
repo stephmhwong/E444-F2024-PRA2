361007eb165eda3029bbe7103983afceec82d93b
Stephanie Wong <stephmh.wong@hotmail.com>
2024-09-24 17:42:16 -0400

    add base.html for template work with added date-time

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
@@ -7,4 +14,4 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
