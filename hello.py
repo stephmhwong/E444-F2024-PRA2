9fd5916666cc87a561edb19232a748075b80b6a0
Stephanie Wong <stephmh.wong@hotmail.com>
2024-09-24 21:14:57 -0400

    made some changes so that the the nav-bar ad date-time are in index.html

--- hello.py
+++ hello.py
@@ -10,8 +10,8 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name, current_time=datetime.utcnow())
+    return render_template('user.html', name=name)
